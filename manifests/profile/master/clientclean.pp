8a8fb12fa2a2170514854203bcd1981160c6c24d
Johannes <root@cloud18.bki1.sys11.net>
2014-09-04 10:34:32 +0200

    Fixed master side of clientclean.

--- manifests/profile/master/clientclean.pp
+++ manifests/profile/master/clientclean.pp
@@ -20,12 +20,12 @@ class sys11puppet::profile::master::clientclean(
     path   => '/etc/default/stunnel4',
     line   => 'ENABLED=1',
     match  => '^ENABLED=',
-    notify => Service['stunnel4'],
   }
 
   service {'stunnel4':
-    ensure => running,
-    enable => true,
+    ensure  => running,
+    enable  => true,
+    require => File_Line['enable_stunnel4'],
   }
 }
 
