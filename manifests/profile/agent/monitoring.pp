108efc217842ffda1b77202b44c638904a623d91
Stefan Andres <s.andres@syseleven.de>
2014-11-14 13:18:14 +0100

    add puppet monitoring

---
+++ manifests/profile/agent/monitoring.pp
@@ -0,0 +1,24 @@
+class sys11puppet::profile::agent::monitoring(
+  $puppet_master = hiera('sys11puppet::common::puppet_master'),
+  $monitoring       = hiera('sys11stack::monitoring', false),
+) {
+  # only run when run via puppet-master
+  if $::clientcert {
+    case $monitoring {
+      'sensu':  {
+        file {'/usr/lib/nagios/plugins/check_puppet_agent':
+          ensure => file,
+          mode   => 555,
+          source => "puppet:///modules/$module_name/check_puppet_agent",
+        }
+        sensu::check{'check_puppet_agent':
+          command => "PATH=\$PATH:/usr/lib/nagios/plugins/ check_puppet_agent",
+          require => File['/usr/lib/nagios/plugins/check_puppet_agent'],
+        }
+      }
+      false:  { }
+      default: { fail("Only sensu monitoring supported ('$monitoring' given)") }
+    }
+
+  }
+}
