b6274371aae00fe71d66cf80977b77bb3c742808
Johannes Grassler <j.grassler@syseleven.de>
2015-03-02 17:03:32 +0100

    os-396 Do not declare /etc/init/puppetmaster-clean-certificate.conf on puppet master.

--- manifests/profile/agent/clientclean.pp
+++ manifests/profile/agent/clientclean.pp
@@ -1,8 +1,10 @@
 class sys11puppet::profile::agent::clientclean(
   $puppet_master = hiera('sys11puppet::common::puppet_master'),
 ) {
-  file {'/etc/init/puppetmaster-clean-certificate.conf':
-    ensure  => file,
-    content => template("$module_name/upstart/puppetmaster-clean-certificate.conf.erb"),
+  if $::fqdn != $puppet_master {
+    file {'/etc/init/puppetmaster-clean-certificate.conf':
+      ensure  => file,
+      content => template("$module_name/upstart/puppetmaster-clean-certificate.conf.erb"),
+    }
   }
 }
