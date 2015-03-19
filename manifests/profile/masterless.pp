b1aaeffa2facef06490f7d34872d561809b9f620
Johannes Grassler <j.grassler@syseleven.de>
2015-03-19 16:14:43 +0100

    os-465 Added removal for noop/regular cronjob depending on noop parameter.

--- manifests/profile/masterless.pp
+++ manifests/profile/masterless.pp
@@ -2,18 +2,41 @@ class sys11puppet::profile::masterless(
   $runinterval = hiera('sys11puppet::masterless::runinterval', 5),
   $noopvalue = hiera('sys11puppet::agent::noop', false),
 ) {
+    $command_noop = '/usr/local/sbin/run_puppet_hiera --logdest syslog'
+    $command = '/usr/local/sbin/run_puppet_hiera --logdest syslog'
 
   if $noop {
-    $command = '/usr/local/sbin/run_puppet_hiera --logdest syslog'
+    # Remove normal command
+    cron{'run_puppet_hiera':
+      ensure  => absent,
+      command => $command,
+      user    => root,
+      minute  => "*/${runinterval}"
+      }
+
+    # Add normal command
+    cron{'run_puppet_hiera':
+      command => $command_noop,
+      user    => root,
+      minute  => "*/${runinterval}"
+      }
   }
   else {
-    $command = '/usr/local/sbin/run_puppet_hiera --logdest syslog'
+    # Add normal command
+    cron{'run_puppet_hiera':
+      command => $command,
+      user    => root,
+      minute  => "*/${runinterval}"
+      }
+
+    # Remove noop command
+    cron{'run_puppet_hiera':
+      ensure  => absent,
+      command => $command_noop,
+      user    => root,
+      minute  => "*/${runinterval}"
+      }
   }
 
-  cron{'run_puppet_hiera':
-    command => 
-    user    => root,
-    minute  => "*/${runinterval}"
-    }
 }
 
