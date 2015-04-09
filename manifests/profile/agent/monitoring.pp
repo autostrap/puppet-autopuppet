3820b70a340562648695a6e81ed00db8ef678a87
Christoph Glaubitz <c.glaubitz@syseleven.de>
2015-04-09 12:22:43 +0200

    os-512 renamed check_XYZ to XZY

--- manifests/profile/agent/monitoring.pp
+++ manifests/profile/agent/monitoring.pp
@@ -21,8 +21,13 @@ class sys11puppet::profile::agent::monitoring(
           require => File['/usr/lib/nagios/plugins/check_puppet_agent'],
         }
 
+        # there is no purge on type sensu_check
+        sensu::check{ 'check_puppet_agent':
+          ensure  => absent,
+          command => '/dev/null',
+        }
 
-        sensu::check{'check_puppet_agent':
+        sensu::check{ 'puppet_agent':
           command     => 'sudo /usr/lib/nagios/plugins/check_puppet_agent',
           require     => [File['/usr/lib/nagios/plugins/check_puppet_agent'], File_line['sudo_check_puppet_agent']],
           interval    => 600,
