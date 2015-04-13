eb6ec2be7f2caacb87f91cf4d209a9351859fc1b
Johannes Grassler <j.grassler@syseleven.de>
2015-04-13 10:04:05 +0200

    os-518 Changed in-code default for reportclean maximum age to 4 days.

--- manifests/profile/reportclean.pp
+++ manifests/profile/reportclean.pp
@@ -1,7 +1,7 @@
 # Ensures reports are cleaned up periodically since they will quickly use up lots of disk space.
 class sys11puppet::profile::reportclean(
   $clean_reports = hiera('sys11puppet::clean_reports', false),
-  $max_age = hiera('sys11puppet::report_max_age', 7),  # Maximum allowed age for reports in days (must be >= 1).
+  $max_age = hiera('sys11puppet::report_max_age', 4),  # Maximum allowed age for reports in days (must be >= 1).
 ) {
   if $clean_reports {
     cron{'cleanup-puppet-reports':
