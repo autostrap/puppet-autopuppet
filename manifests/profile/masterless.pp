aa15ca66e4b90968f82794c5917941e63b8a630e
Stefan Andres <s.andres@syseleven.de>
2015-09-11 16:59:07 +0200

    fix masterless cronjob output spam

--- manifests/profile/masterless.pp
+++ manifests/profile/masterless.pp
@@ -2,8 +2,8 @@ class sys11puppet::profile::masterless(
   $runinterval = hiera('sys11puppet::masterless::runinterval', 5),
   $noopvalue = hiera('sys11puppet::masterless::noop', false),
 ) {
-    $command_noop = '/usr/local/sbin/run_puppet_hiera --noop --logdest syslog'
-    $command = '/usr/local/sbin/run_puppet_hiera --logdest syslog'
+    $command_noop = '/usr/local/sbin/run_puppet_hiera --noop --logdest syslog >/dev/null 2>&1'
+    $command = '/usr/local/sbin/run_puppet_hiera --logdest syslog >/dev/null 2>&1'
     $path = 'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 
   if $noopvalue {
