ef5f0d42baa6b42b6d9ecc6e9acd25bbd502bb8a
Johannes Grassler <j.grassler@syseleven.de>
2015-09-30 16:03:09 +0200

    Revert "os-942 Added agent_bootstrap role (without monitoring)."
    This reverts commit ba321b9311b3b385c2c0efb8a64ec24f305f509b: turns out
    we do not need it, after all.

--- manifests/role/agent_bootstrap.pp
+++
@@ -1,6 +0,0 @@
-class sys11puppet::role::agent() {
-  # os-395, use sys11puppet::role::puppetmaster and ::agent in combination
-  contain puppet::repo::puppetlabs
-
-  class {'sys11puppet::profile::agent':}
-}
