7e3ccfb6cadcd0bd46756850911deaf5d4a94b5c
Johannes Grassler <j.grassler@syseleven.de>
2015-09-30 16:32:54 +0200

    Revert "os-942 Removed obsolete puppetmaster_bootstrap role."
    This reverts commit 7ef8e7935dadf99016d7959ae324b55ae339c03b: the role
    is not obsolete after all, since it fixes the Apache MPM issue from
    os-818 apart from the monitoring issue.

---
+++ manifests/role/puppetmaster_bootstrap.pp
@@ -0,0 +1,17 @@
+# Sets up a puppet master.
+class sys11puppet::role::puppetmaster_bootstrap(
+  $enable_dashboard = hiera('sys11puppet::dashboard::enable'),
+) {
+  # os-395, use sys11puppet::role::puppetmaster and ::agent in combination
+  contain puppet::repo::puppetlabs
+  include sys11puppet::profile::reportclean
+  require sys11puppet::profile::master::apache_mpm
+
+  class {'sys11puppet::profile::master::main':}
+
+  if $enable_dashboard {
+    class {'sys11puppet::profile::dashboard':
+      require => Class['sys11puppet::profile::master'],
+    }
+  }
+}
